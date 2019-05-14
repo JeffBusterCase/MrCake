7b0097bac3a1c8cbe6002f81dec69e0f8a77464a
Aluno <jefferson_bomfim@outlook.com>
2019-05-14 13:33:46 -0300

    Produtos funcionando

--- adm_for/produtos.php
+++ adm_for/produtos.php
@@ -1,6 +1,6 @@
 <div class="tab-pane fade" id="pills-produtos" role="tabpanel" aria-labelledby="pills-produtos-tab">
-	<div class="container" style="margin-top: 50px">
-		<h2>Meus Produtos</h2>
+    <div class="container" style="margin-top: 50px">
+        <h2>Meus Produtos</h2>
         <table class="table table-striped">
             <thead>
             <tr>
@@ -11,42 +11,30 @@
             </tr>
             </thead>
             <tbody>
-
             <?php
-			
+               $sql = new sqlFornecedor();
+
                $produtos = $sql->getProdutos($email);
-				echo var_dump($produtos);
-			   //$len = count($produtos);
-			
-               //if($len > 0) {
-			      //foreach($produtos as $produto) {
-					echo "<tr>
-							<td style='vertical-align:middle'> 
-								<img src='$produto->imagem'>
-							</td>
-							<td style='vertical-align:middle'> 
-								$produto->nome
-							</td>
-							<td style='vertical-align:middle'>
-								$produto->descricao
-							</td>
-							<td style='vertical-align:middle'>
-								R$ $produto->preco 
-							</td>
-							<td>
-								<form  method = 'post' class='prod' style='display:none;'>
-									<input type = 'text' name='i' value = '1' style='display:none'/>
-									<input type = 'text' name='acao' value='eproduto' style='display:none'/>
-									<input type = 'submit' class='btn btn-sm btn-deep-orange' id='btneditar1' value='Editar'>
-								</form>
-							</td>
-						</tr>";
-				  //}
-			   //}
+
+               foreach($produtos as $produto){
+            ?>
+            <tr>
+                <td style="vertical-align:middle"><?php echo $produto->nome ?></td>
+                <td style="vertical-align:middle"><?php echo $produto->descricao ?></td>
+                <td style="vertical-align:middle">R$ <?php echo $produto->preco ?></td>
+                <td>
+                    <form action = "" method = "post" class = "prod" style="display:none;">
+                        <input type = "text" name = "i" value = "1" style="display:none"/>
+                        <input type = "text" name = "acao" value = "eproduto" style="display:none"/>
+                        <input type = "submit" class="btn btn-sm btn-deep-orange" id = "btneditar1" value = "Editar">
+                    </form>
+                </td>
+            </tr>
+            <?php
+               }
                ?>
             </tbody>
         </table>
-        <div id = "recebeDados">
-		</div>
+        <div id = "recebeDados"></div>
     </div>
 </div>
